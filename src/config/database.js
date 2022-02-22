15cde517e5954a591b1cc081d698b367a4b56e6d
Thomas Bouasli <thomas.bouasli@gmail.com>
2022-02-22 08:28:54 -0300

    [feat] Created config file

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: 'postgres',
+    host: '	abul.db.elephantsql.com',
+    username: 'ylwkrwda',
+    password: 'ClorAJsmNdUprJRBfsvFRVUM2EsS8rmx',
+    database: 'ylwkrwda',
+    define: {
+        timestamps: true,
+        underscored: true,
+    },
+};
