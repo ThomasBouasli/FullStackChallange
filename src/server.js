94f3f0625a7e3a12185c2f2b9c40d0727cc4cf14
Thomas Bouasli <thomas.bouasli@gmail.com>
2022-02-22 08:26:57 -0300

    [feat] Added backend dependencies

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const Express = require("express");
+const routes = require("./routes");
+
+const app = Express();
+
+app.use(Express.json());
+app.use(routes);
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
