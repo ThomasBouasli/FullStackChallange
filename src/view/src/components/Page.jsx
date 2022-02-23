ae21eb0066c437e4481faa2a6ec638f0be1c74f0
Thomas Bouasli <thomas.bouasli@gmail.com>
2022-02-23 16:44:11 -0300

    [fix] Display issue with bigger displays

--- src/view/src/components/Page.jsx
+++ src/view/src/components/Page.jsx
@@ -1,26 +1,28 @@
-import { Container } from "@mui/material";
+import { Box } from "@mui/material";
 
 export default function Page(props) {
   return (
-    <Container
+    <Box
       sx={{
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
         justifyContent: "space-around",
-        height: "100vh",
+        height: "70vh",
         width: "100vw",
         padding: "10vh 0 20vh",
+        margin: "0 0",
         backgroundImage: `url(https://i.ibb.co/gDyMJ13/541732.png)`,
         backgroundRepeat: "no-repeat",
         backgroundSize: "50%",
         backgroundPosition: props.position,
         backgroundOrigin: "padding-box",
+        maxWidth: "100vw",
       }}
 
       maxWidth="xl"
     >
       {props.children}
-    </Container>
+    </Box>
   );
 }
