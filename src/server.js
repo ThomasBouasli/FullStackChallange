d2bf05549e73af0caada5c76adf48dc6f0281c80
Thomas Bouasli <thomas.bouasli@gmail.com>
2022-02-23 11:54:14 -0300

    [feat] Added view path

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 require('./database');
+const path = require('path');
 const Express = require("express");
 const routes = require("./routes");
 
 const app = Express();
 
 app.use(Express.json());
-app.use(routes);
+app.use("/api", routes);
+app.use(Express.static(path.resolve(__dirname, "view", "build")));
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
