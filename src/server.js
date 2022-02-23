73538fc8adcea9ea25fb6a1cd25747251789b5e4
Thomas Bouasli <thomas.bouasli@gmail.com>
2022-02-23 12:24:05 -0300

    [fix] Heroku Port binding

--- src/server.js
+++ src/server.js
@@ -9,6 +9,6 @@ app.use(Express.json());
 app.use("/api", routes);
 app.use(Express.static(path.resolve(__dirname, "view", "build")));
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server is running on port 3000");
 });
