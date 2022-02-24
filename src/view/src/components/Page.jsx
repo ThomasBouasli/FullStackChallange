21e6d8afb7946368e30d2192440c0376ef329b2a
Thomas Bouasli <thomas.bouasli@gmail.com>
2022-02-23 21:29:46 -0300

    [feat] Added media breaks

--- src/view/src/components/Page.jsx
+++ src/view/src/components/Page.jsx
@@ -1,6 +1,11 @@
 import { Box } from "@mui/material";
+import useMediaQuery from '@mui/material/useMediaQuery';
+
 
 export default function Page(props) {
+
+  const isMobile = useMediaQuery('(max-width: 600px)');
+
   return (
     <Box
       sx={{
@@ -8,14 +13,14 @@ export default function Page(props) {
         flexDirection: "column",
         alignItems: "center",
         justifyContent: "space-around",
-        height: "70vh",
+        height: isMobile ? "70vh" : "85vh",
         width: "100vw",
-        padding: "10vh 0 20vh",
+        padding: isMobile ? "10vh 0 20vh" : "5vh 0 10vh",
         margin: "0 0",
         backgroundImage: `url(https://i.ibb.co/gDyMJ13/541732.png)`,
         backgroundRepeat: "no-repeat",
         backgroundSize: "50%",
-        backgroundPosition: props.position,
+        backgroundPosition: isMobile ? props.position : "-60% -20%",
         backgroundOrigin: "padding-box",
         maxWidth: "100vw",
       }}
--- src/view/src/routes/home.jsx
+++ src/view/src/routes/home.jsx
@@ -1,13 +1,29 @@
 import { useState, useEffect } from "react";
 import Page from "../components/Page";
 
+import useMediaQuery from "@mui/material/useMediaQuery";
+
 import { TailSpin } from "react-loader-spinner";
 import { useNavigate } from "react-router-dom";
 import { Button, Box } from "@mui/material";
 
 export default function Home() {
-  const [cookies, setCookies] = useState(null);
+  const [cookies, setCookies] = useState([
+    {
+      id: 1,
+      name: "Chocolate Chip",
+      image: "https://i.ibb.co/gDyMJ13/541732.png",
+      price: "$1.00",
+    },
+    {
+      id: 2,
+      name: "Chocolate Chip",
+      image: "https://i.ibb.co/gDyMJ13/541732.png",
+      price: "$1.00",
+    },
+  ]);
   const navigate = useNavigate();
+  const isMobile = useMediaQuery("(max-width: 600px)");
 
   useEffect(() => {
     fetchData();
@@ -16,7 +32,7 @@ export default function Home() {
       const result = await fetch("/api/products", {
         method: "GET",
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
           Authorization: `Bearer ${localStorage.getItem("token")}`,
         },
       });
@@ -35,31 +51,34 @@ export default function Home() {
   return (
     <Page position="150% -10%">
       <h1>Cookies</h1>
-      <Box sx={{
-        overflowY: "auto",
-        padding: "0px 5%",
-        boxSizing: "border-box",
-      }}>
-      {cookies == null ? (
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <TailSpin color="#D5A150" height={80} width={80} />
-          <span>Fetching some Cookies...</span>
-        </div>
-      ) : (
-        cookies.map((cookie) => (
-          <Cookie imgSrc={cookie.image} name={cookie.name} />
-        ))
-      )}
+      <Box
+        sx={{
+          overflowY: "auto",
+          padding: "0px 5%",
+          boxSizing: "border-box",
+          height: isMobile ? "auto" : "100%",
+        }}
+      >
+        {cookies == null ? (
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <TailSpin color="#D5A150" height={80} width={80} />
+            <span>Fetching some Cookies...</span>
+          </div>
+        ) : (
+          cookies.map((cookie) => (
+            <Cookie key={cookie.id} imgSrc={cookie.image} name={cookie.name} price={cookie.price} />
+          ))
+        )}
       </Box>
       <Button
         sx={{ paddingX: "3rem", marginY: "1rem", backgroundColor: "#D5A150" }}
-        size="large"S
+        size="large"
         variant="contained"
         color="warning"
         onClick={() => {
@@ -73,16 +92,21 @@ export default function Home() {
   );
 }
 
-function Cookie({ imgSrc, name }) {
+function Cookie({ imgSrc, name, price, id }) {
+  const isMobile = useMediaQuery("(max-width: 600px)");
+
   return (
     <div
+    key={id}
       style={{
         display: "flex",
         alignItems: "center",
+        justifyContent: "space-between",
         boxShadow: "4px 4px 0px #000000",
         padding: "0.5rem 2rem",
         borderRadius: "0.5rem",
-        margin: "2rem 0px",
+        margin: isMobile ? "2rem 0px" : "3rem 0px",
+        width: isMobile ? "auto" : "min(35vw, 300px)",
       }}
     >
       <img
@@ -91,6 +115,9 @@ function Cookie({ imgSrc, name }) {
         style={{ height: "40px", marginRight: "1rem" }}
       />
       <span>{name}</span>
+      {
+        isMobile ? null : <span>{price}</span>
+      }
     </div>
   );
 }
